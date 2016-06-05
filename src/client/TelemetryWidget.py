0d4fc189baf134b4334feca21a352d3e507e1c33
Nathan Dileas <ndileas@gmail.com>
2016-06-05 14:38:42 -0400

    Added methods and functionality to telemtry widget.

--- src/client/TelemetryWidget.py
+++ src/client/TelemetryWidget.py
@@ -6,6 +6,17 @@ class TelemetryWidget(tk.Frame):
         self.frame = tk.Frame()
         tk.Frame.__init__(self)
         self.parent = parent
+
+        self.labels = []        
+        self.label_text = ['RunClock: %s', 'Distance: %s', 'Speed: %s', 'Power: %s', 'Battery Remaining: %s', 'Integration Time: %s', 'Connected: %s', 'RPM: %s', 'System Voltage: %s', 'System Current: %s']
         
-        label1 = tk.Label(master=root, text="")
-        label1.grid()
+        for _text in label_text:
+            self.labels.append(tk.Label(master=root, text=_text % ' '))
+
+        for label in self.labels:   # put them all in a row
+            label.grid(self.labels.index(label) + 1, 1)
+        
+    def update(telemetry_list):
+        for i in range(10):
+            self.labels[i]['text'] = self.label_text[i] % telemetry_list[i]
+
