7c53e42a6c1e79491d4aa1664f533dc950059003
Nathan Dileas <ndileas@gmail.com>
2016-06-06 09:44:12 -0400

    Changed color assignment for overlay.

--- src/highlight.py
+++ src/highlight.py
@@ -1,27 +1,24 @@
 import numpy
 import cv2
 
-def highlight(img, pixels, channel, alpha=0.7):
+def highlight(img, pixels, color, alpha=0.7):
     """ highlight pixels in image of channel. """
     # img: numpy or opencv 3 channeled image
-    # pixels: 2d boolean array, same shape` as img
-    # channel: channel (or color) to draw in
+    # pixels: 2d boolean or index array
+    # color: 3 item tuple, indicates color of pixel shadin, ex. (0, 0, 255)
     # alpha: how transparent to make the drawing, 0-1 (1 is opaque)
 
     overlay = img.copy()
-    val = numpy.array([0, 0, 0])
-    val[channel] = 255
-    overlay[pixels] = val
+    overlay[pixels] = color
     
-    cv2.addWeighted(overlay, alpha, img, 1 - alpha,
-		0, img)
+    cv2.addWeighted(overlay, alpha, img, 1 - alpha, 0, img)
 
     return img
 
 if __name__=='__main__':
-    img = cv2.imread('elon_2_copy2.jpg',1)
+    img = cv2.imread('test/elon_2_copy2.jpg',1)
 
-    output = highlight(img, numpy.where(img[:,:,0]>200), 2)
+    output = highlight(img, numpy.where(img[:,:,0]>200), (128, 255, 0))
 
     cv2.namedWindow(' ')
     cv2.imshow(' ', output)
