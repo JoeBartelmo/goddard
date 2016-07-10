81b8de498ad57525714f6d25d348c76441d59667
ark9719 <ark9719@rit.edu>
2016-07-10 11:19:00 -0400

    Added/cleaned Jeff's gpio class, need to init pins still

---
+++ mars-controller/Indicate.py
@@ -0,0 +1,70 @@
+import logging
+"""
+
+Usable GPIO pins are as follows
+
+160,161,162,163,164,165,166,57
+
+#EMBED ANY REFERENCE TO THIS IN A TRY STATEMENT
+##ValueError will be generated if there is an invalid gpioPin
+
+
+
+#MUST MODIFY WATCHDOG TO MONITOR AND CHANGE INDICATORS 
+
+create 8 pin objects:
+
+	reset pin (166)
+	connection LED (163)
+	warning LED (164)
+	batt LED (165)
+	relay 1 (57)
+	relay 2 (160)
+	relay 3 (161)
+	relay 4 (162)
+
+
+"""
+
+
+
+gpio_list = [57,160,161,162,163,164,165,166]
+
+class GpioPin():
+
+    def __init__(self, gpioPin, direction = 'out'):
+        self._state = 'undefined'
+        self._direction = direction
+
+        if gpioPin in gpio_list:
+            self._gpioPin = gpioPin
+        else:
+            logging.warning("GPIO PIN SETUP FAILED\r\nGPIO pin {} does not exist".format(str(gpioPin)))
+            logging.warning("GPIO pin must be one of the following {}".format(str(gpio_list)))
+            raise ValueError("invalid GPIO pin number")
+
+
+    def setup(self):
+        #exporting the GPIO pin so it can be accessed
+        with open('/sys/class/gpio/export','w') as export_file:
+            export_file.write(str(self._gpioPin))
+        #setting the pin direction
+        with open('/sys/class/gpio/gpio{}/direction'.format(str(self._gpioPin)),'w') as direction_file:
+            direction_file.write(self._direction)
+
+
+    def changeState(self,state):
+        if self._direction == "out":
+            with open('/sys/class/gpio/gpio{}/value'.format(str(self._gpioPin)),'w') as value_file:
+                value_file.write(str(state))
+        else:
+            raise ValueError("pin must be an output to change it's state")
+
+        self._state = state
+
+
+
+
+
+
+
