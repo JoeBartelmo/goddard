9560b6e37e11b97e476ca001c45dcd62a9d13b82
Nathan Dileas <ndileas@gmail.com>
2016-07-25 19:37:35 -0400

    Changes to project stucture for GUI and image processing folders.
    Former-commit-id: 372728eb0fcf6c0adadca18fdbae15f1f86000e4

--- src/test/auto_canny_benchmark.py
+++
@@ -1,7 +0,0 @@
-from timeit import timeit
-import cv2
-
-img = cv2.imread('/home/nathan/python/PyDetect/src/client/assets/webcam.jpg')
-fast = cv2.FastFeatureDetector()
-
-print timeit('fast.detect(img, None)', setup="from __main__ import img, fast", number=10000)
--- src/test/step_defs/verification_step_defs.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-#
-# @file "verification_step_defs"
-# @authors "Joseph Bartelmo"
-# @contact "joebartelmo@gmail.com"
-# @date "4/23/2016"
-# @copyright "MIT"
-#
-
