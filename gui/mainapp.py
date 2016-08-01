e5d0590fa7776be1d9a4b280969753364b02acab
Nathan Dileas <ndileas@gmail.com>
2016-08-01 18:15:41 -0400

    Implementation of new display m,ethod.
    Former-commit-id: 3ff70bcc5a2046cd44574c594180fea1d2740d14

--- gui/mainapp.py
+++ gui/mainapp.py
@@ -1,58 +1,59 @@
-from TelemetryWidget import TelemetryWidget
-from MasterWidget import MasterWidget
-from ControlWidget import ControlWidget
-from VideoStream import VideoStream
-
+import cv2
+from PIL import Image, ImageTk
+import numpy
 import Tkinter as tk
+from Threads import VideoThread
+from Queue import Queue, Empty
+import time
 
-import sys
+from TelemetryWidget import TelemetryWidget
+from ControlWidget import ControlWidget
+from img_proc.misc import demosaic
 
 class MainApplication(tk.Frame):
-    """
-    Main Window for GUI.
-
-    Consists of several widgets:
-        Telemtry: TelemetryWidget, shows output from valmar
-        Streams: VideoStream, shows video from valmar
-        Stream Control: MasterWidget, controls playback of streams
-        Command: ControlWidget, sends commands and shows logs from valmar
-
-    Args:
-        parent: parent window
-        client_queue_in: queue to get telemetry and logging info
-        client_queue_out: queue to communicate commands
-        server_ip: server IP address for rtsp stream access
-    """
     def __init__(self, parent, client_queue_in, client_queue_out, server_ip):
         tk.Frame.__init__(self, parent)
         self.parent = parent
+        self.stream_order = [0,1,2]
 
         self.init_ui(client_queue_in, client_queue_out, server_ip)
-
+        
         self.start_streams()
-        self.start_telemtry()
+        self.start_telemetry()
 
+        self.after(100, self.display_streams)
+        
     def init_ui(self, client_queue_in, client_queue_out, server_ip):
         """ Initialize visual elements of widget. """
         self.streams = []
 
         print 'Attempting to connect to rtsp stream from left camera'
-        l_src = 'rtsp://' + server_ip + ':8555/'   # left camera setup
-        l = VideoStream(self, l_src, 'Left')
-        l.grid(row=0, column=0)
+        l_src = 'rtsp://' + server_ip + ':8554/'   # left camera setup
+        l = VideoThread(cv2.VideoCapture(l_src),  Queue())
         self.streams.append(l)
 
         print 'Attempting to connect to rtsp stream from centre camera'
-        c_src = 'rtsp://' + server_ip + ':8554/'   # center camera setup
-        c = VideoStream(self, c_src, 'Center', frame_size=(640,480))
-        c.grid(row=2, column=0, columnspan=2, rowspan=2)
+        c_src = 'rtsp://' + server_ip + ':8555/'   # center camera setup
+        c = VideoThread(cv2.VideoCapture(c_src),  Queue())
         self.streams.append(c)
 
         print 'Attempting to connect to rtsp stream from right camera'
         r_src = 'rtsp://' + server_ip + ':8556/'   # right camera setup
-        r = VideoStream(self, r_src, 'Right')
-        r.grid(row=0, column=1)
+        r = VideoThread(cv2.VideoCapture(r_src),  Queue())
         self.streams.append(r)
+
+        # Image display label
+        self.initial_im = tk.PhotoImage()
+        self.image_label = tk.Label(self, image=self.initial_im, width=640,height=720)
+        self.image_label.grid(row=0, column=0, rowspan=2)
+
+        # telemetry display widget
+        self.telemetry_w = TelemetryWidget(self, client_queue_in)
+        self.telemetry_w.grid(row=0, column=1, rowspan=3)
+
+        # valmar control and logging widget
+        self.command_w = ControlWidget(self, client_queue_out)
+        self.command_w.grid(row=0, column=2, rowspan=1)
 
         # radiobuttons for choosing which stream is in focus
         buttons = ['Left', 'Center', 'Right']
@@ -63,83 +64,116 @@ class MainApplication(tk.Frame):
         self.stream_active.set(1)
         for text in buttons:
             b = tk.Radiobutton(frame, text=text, variable=self.stream_active, value=col,\
-                         command=self.show_stream)
+                         command=self.choose_focus)
             b.grid(row=0, column=col, padx=5, pady=5)
             col += 1
         frame.grid(row=1, column=0)
 
-        # stream control widget
-        self.master_w = MasterWidget(self, self.streams)
-        self.master_w.grid(row=1, column=1)
+        self.grid()
 
-        # telemetry display widget
-        self.telemetry_w = TelemetryWidget(self, client_queue_in)
-        self.telemetry_w.grid(row=0, column=4, rowspan=3)
+    def choose_focus(self):
+        """ Change stream focus. """
+        if self.stream_active.get() == 0:  # center focus
+            self.stream_order = [0,1,2]
+        elif self.stream_active.get() == 1:  # left focus
+            self.stream_order = [1,0,2]
+        elif self.stream_active.get() == 2:  # right focus
+            self.stream_order = [0,2,1]
+        else:
+            self.stream_order = [0,1,2]
+    
+    def addText(self, l_frame, c_frame, r_frame):
+        """ Change text on boxes """
+        font = cv2.FONT_HERSHEY_SIMPLEX
+        center = (10, 390)
+        topleft = (10, 230)
+        topright = (10, 230)
 
-        # valmar control and logging widget
-        self.command_w = ControlWidget(self, client_queue_out)
-        self.command_w.grid(row=3, column=4, rowspan=1)
+        def setText(l_text, r_text, c_text):
+            cv2.putText(l_frame, l_text, topleft, font, 0.5, (255,0,0), 1)
+            cv2.putText(c_frame, c_text, center, font, 0.5, (255,0,0), 1)
+            cv2.putText(r_frame, r_text, topright, font, 0.5, (255,0,0), 1)
 
-        r, c = self.grid_size()
-        for i in range(r):
-            self.grid_rowconfigure(i, pad=5)
+        if self.stream_active.get() == 0:  # left focus
+            setText('Center', 'Right', 'Left')
+        elif self.stream_active.get() == 1:  # center focus
+            setText('Left', 'Right', 'Center')
+        elif self.stream_active.get() == 2:  # right focus
+            setText('Right', 'Center', 'Left')
 
-        for i in range(c):
-            self.grid_columnconfigure(i, pad=5)
+        return (l_frame, c_frame, r_frame)
 
-        self.grid(sticky='e')
+    def display_streams(self, delay=0):
+        a, b, c = self.stream_order
+
+        l_frame = demosaic(self.streams[a]._queue.get())
+        c_frame = demosaic(self.streams[b]._queue.get())
+        r_frame = demosaic(self.streams[c]._queue.get())
+
+        l_frame = cv2.resize(l_frame, (320, 240))
+        c_frame = cv2.resize(c_frame, (640, 480))
+        r_frame = cv2.resize(r_frame, (320, 240))
+
+        self.addText(l_frame, c_frame, r_frame)
+
+        big_frame = numpy.zeros((720,640,3))
+        big_frame[:240,:320,:] = l_frame
+        big_frame[:240,320:640,:] = r_frame
+        big_frame[240:, :,:] = c_frame
+
+        big_frame = numpy.asarray(big_frame, dtype=numpy.uint8)
+
+        a = Image.fromarray(big_frame)
+        b = ImageTk.PhotoImage(image=a)
+        self.image_label.configure(image=b, width=big_frame.shape[0], height=big_frame.shape[1])
+        self.image_label._image_cache = b  # avoid garbage collection
+
+        self.update()
+        self.after(delay, self.display_streams, delay)
 
     def start_streams(self):
-        """ start frame capture from streams. """
-        for stream in self.streams:
-            if stream.vidcap.isOpened():
-                stream.vthread.start()
+        for s in self.streams:
+            if s._vidcap.isOpened():
+                s.start()
 
-    def start_telemtry(self):
+    def start_telemetry(self):
         """ after 5 seconds, start telemtry updates. """
         self.telemetry_w.tthread.start()
         #self.after(5000, self.telemetry_w.persistent_update)   # TODO decide on appropriate interval
 
-    def show_stream(self):
-        """ Change stream focus. """
-        for s in self.streams:
-            s.grid_forget()
-
-        if self.stream_active.get() == 0:  # left focus
-            self.stream_rearrange(order=[0,1,2])
-        elif self.stream_active.get() == 1:  # center focus
-            self.stream_rearrange(order=[1,0,2])
-        elif self.stream_active.get() == 2:  # right focus
-            self.stream_rearrange(order=[2,0,1])
-    
-        self.update()
-
-    def stream_rearrange(self, order=[1,0,2]):
-        a, b, c = order
-        self.streams[a].frame_size = (640,480)
-        self.streams[a].image_label.config(width=640, height=480)
-        self.streams[a].grid(row=2, column=0, columnspan=2, rowspan=2)
-
-        self.streams[b].frame_size = (320,240)
-        self.streams[b].image_label.config(width=320, height=240)
-        self.streams[b].grid(row=0, column=0)
-
-        self.streams[c].frame_size = (320,240)
-        self.streams[c].image_label.config(width=320, height=240)
-        self.streams[c].grid(row=0, column=1)
-        
     def close_(self):
-        """ Customized quit function to allow for safe closure of processes. """
-        self.master_w.quit()
         self.telemetry_w.quit_()
 
         for stream in self.streams:
-            stream.quit_()
-        print 'asdf'
+            stream.stop()
+            stream.join()
+
         self.quit()
-        print 'asdf2'
         self.parent.quit()
-        print 'asdf3'
         self.parent.destroy()
-        print 'asdf4'
+            
+
+if __name__ == '__main__':
+    root = tk.Tk()   # get root window
+    server_ip = 'hyperlooptk1.student.rit.edu'
+    in_queue = Queue()
+    out_queue = Queue()
+
+    # define mainapp instance
+    m = MainApplication(root, in_queue, out_queue, server_ip)
+
+    # run forever
+    root.mainloop()
+    """
+    stream = 'rtsp://129.21.56.100:8556/'
+
+    vid = cv2.VideoCapture(stream)
+
+    while vid.isOpened() is True:
+        fl, fr = vid.read()
+        if fl:
+            cv2.imshow(stream, fr)
+            cv2.waitKey(1)
+    """
+
 
