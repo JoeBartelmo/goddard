bf2bcabcfae0d9f65c2f3d121bb6c28e8a8900b3
Joe Bartelmo <joebartelmo@gmail.com>
2016-08-05 20:02:57 +0000

    Add Json file handling for options
    Former-commit-id: 016b6c1acd32da2c1a761f649224392ff4c869f9

--- valmar/capturePipe.cpp
+++ valmar/capturePipe.cpp
@@ -1,4 +1,6 @@
 #include "stdafx.h"
+#include "xiApiPlusOcv.hpp"
+#include "json.hpp"
 
 #include <m3api/xiApi.h>
 #include <memory.h>
@@ -10,62 +12,128 @@
 #include <fcntl.h>
 #include <sys/stat.h>
 #include <sys/types.h>
+#include <fstream>
 
-#define HandleResult(res,place) if (res!=XI_OK) {printf("Error after %s (%d)\n",place,res);goto finish;}
+#define FIFO_LOCATION "CppToPythonFifo"
 
-char fifoLocation[] = "CppToPythonFifo";
+using json = nlohmann::json;
+using namespace cv;
 
-int _tmain(int argc, _TCHAR* argv[])
+//http://docs.opencv.org/2.4/doc/tutorials/highgui/video-input-psnr-ssim/video-input-psnr-ssim.html#image-similarity-psnr-and-ssim
+double getPSNR(const Mat& I1, const Mat& I2)
 {
-    
-    // image buffer
-    XI_IMG image;
-    memset(&image,0,sizeof(image));
-    image.size = sizeof(XI_IMG);
+    Mat s1;
+    absdiff(I1, I2, s1);       // |I1 - I2|
+    s1.convertTo(s1, CV_32F);  // cannot make a square on 8 bits
+    s1 = s1.mul(s1);           // |I1 - I2|^2
 
-    // Sample for XIMEA API V4.05
-    HANDLE xiH = NULL;
-    XI_RETURN stat = XI_OK;
+    Scalar s = sum(s1);         // sum elements per channel
+
+    double sse = s.val[0] + s.val[1] + s.val[2]; // sum channels
+
+    if( sse <= 1e-10) // for small values return zero
+        return 0;
+    else
+    {
+        double  mse =sse / (double)(I1.channels() * I1.total());
+        double psnr = 10.0 * log10((255 * 255) / mse);
+        return psnr;
+    }
+}
+
+int sendImagesToPipe(xiAPIplusCameraOcv *cam, int images_to_load) {
+    printf("PSNR Threshold met, sending next %d frames\n", images_to_load);
+    int fifo, img_size, frameIndex;
+    Mat cv_mat_image;
     
-    // Retrieving a handle to the camera device 
-    printf("Opening first camera...\n");
-    stat = xiOpenDevice(0, &xiH);
-    HandleResult(stat,"xiOpenDevice");
-    
-    // Setting "exposure" parameter (10ms=10000us)
-    stat = xiSetParamInt(xiH, XI_PRM_EXPOSURE, 10000);
-    HandleResult(stat,"xiSetParam (exposure set)");
-    
-    // Note:
-    // The default parameters of each camera might be different in different API versions
-    // In order to ensure that your application will have camera in expected state,
-    // please set all parameters expected by your application to required value.
-    
-    printf("Starting acquisition...\n");
-    stat = xiStartAcquisition(xiH);
-    HandleResult(stat,"xiStartAcquisition");
-    
-    while(1) {
-        // getting image from camera
-        //We want to open the pipe before we start our stream.
-        //NOTE: the pipe is halting, will not continue until receiving end is
-        //      connected
-        mkfifo(fifoLocation, 0666);
+    printf("Waiting for receiving end of pipe to connect to valmar...\n");
+    for(frameIndex = 0; frameIndex < images_to_load; frameIndex++) {
         //this is the blocking operation
-        printf("Waiting for receiving end of pipe to connect to valmar...\n");
-        int fifo = open(fifoLocation, O_WRONLY);
-        stat = xiGetImage(xiH, 5000, &image);
-        HandleResult(stat,"xiGetImage");
-        printf("Attempting to write image to fifo\n");
-        write(fifo, image.bp, image.bp_size);
+        cv_mat_image = (*cam).GetNextImageOcvMat();
+        fifo = open(FIFO_LOCATION, O_WRONLY);
+        img_size = cv_mat_image.total() * cv_mat_image.elemSize();
+        printf("Image size of cv::MAT is %d bytes\n", img_size);
+        write(fifo, cv_mat_image.data, img_size);
         close(fifo);
     }
+    return 1;
+}
+
+json readFileAsJson(string fileName) {
+    printf("Attempting to load in file %s\n", fileName.c_str());
+    std::ifstream t(fileName);
+    std::stringstream buffer;
+    buffer << t.rdbuf();
+    json returnedJSON = json::parse(buffer.str());
+    return returnedJSON;
+}
+
+int _tmain(int argc, _TCHAR* argv[])
+{ 
+    //load settings
+    string settingsFile = "command.json";
+    switch(argc) {
+        case 1:
+            break;
+        case 2:
+            settingsFile = argv[1];
+            break;
+        default:
+            printf("Usage: capturePipe [optional settings file.json]%d", argc);
+            return EXIT_FAILURE;
+    }
+
+    json settings = readFileAsJson(settingsFile);
+
+    if(settings["command"]["enabled"] == false) {
+        return EXIT_SUCCESS;
+    }
     
-    printf("Stopping acquisition...\n");
-    xiStopAcquisition(xiH);
-    xiCloseDevice(xiH);
-finish:
+    // Sample for XIMEA OpenCV
+    xiAPIplusCameraOcv cam;
+
+    // Retrieving a handle to the camera device
+    printf("Opening first camera...\n");
+    cam.OpenFirst();
+    
+    //Set exposure
+    cam.SetExposureTime(settings["capture"]["exposure_us"]); //10000 us = 10 ms
+    cam.SetGain(settings["capture"]["gain"]);
+    //cam.SetFrameRate(settings["capture"]["framerate"]);
+    cam.SetGammaLuminosity(settings["capture"]["gamma_y"]);
+    cam.SetSharpness(settings["capture"]["sharpness"]);
+    // Note: The default parameters of each camera might be different in different API versions
+    
+    printf("Starting acquisition...\n");
+    cam.StartAcquisition();
+        
+    //We want to open the pipe before we start our stream.
+    //NOTE: the pipe is halting, will not continue until receiving end is
+    //      connected
+    mkfifo(FIFO_LOCATION, 0666);
+   
+    printf("Waiting for PSNR Trigger...\n");
+    while(1) {
+        // getting image from camera
+        try {
+            const Mat mat1 = cam.GetNextImageOcvMat();
+            const Mat mat2 = cam.GetNextImageOcvMat();
+            
+            double psnr;
+            if ((psnr = getPSNR(mat1, mat2)) >= (double)settings["processing"]["psnr_threshold"]) {
+                printf("PSNR of %f reached threshold of %d\n", psnr, (int)settings["processing"]["psnr_threshold"]);
+                sendImagesToPipe(&cam, settings["processing"]["num_frames_to_process"]);
+            }
+        }
+	catch(xiAPIplus_Exception& exp){
+            printf("Error:\n");
+            exp.PrintError();
+            cvWaitKey(200);
+	}
+    }
+    
+    cam.StopAcquisition();
+    cam.Close();
     printf("Done\n");
-    return 0;
 }
 
