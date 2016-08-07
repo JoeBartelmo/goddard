4350893d60574472fde099eef30acfe298e6071a
Joe Bartelmo <joebartelmo@gmail.com>
2016-08-07 22:09:15 +0000

    Add threads for fileforwarding and the folder
    Former-commit-id: b944f5589344071e6768588efeee7145d5db9948

---
+++ server/fileForwarder.py
@@ -0,0 +1,49 @@
+import threading
+import time
+import os
+import socket
+import logging
+import struct
+import base64
+
+logger = logging.getLogger('mars_logging')
+directoryToScan = '../mars/files/'
+
+class FileRelay(threading.Thread):
+    def __init__(self, serverAddr, port):
+        super(FileRelay, self).__init__()
+        self._stop = threading.Event()
+        self._socket = socket.create_connection((serverAddr, port))
+
+    def run(self):
+        while self.stopped() is False:
+            filesToSend = os.listdir(directoryToScan)
+            filesToSend.remove('.gitignore')#we need gitignore to commit this folder >.>
+            if len(filesToSend) > 0:
+                for filename in filesToSend:
+                    with open(directoryToScan + filename, 'rb') as f:
+                        fileInBytes = f.read()
+                    logger.info('Sending file ' + filename + ' to the client')
+                    
+                    intPacker = struct.Struct('I')
+                    
+                    value = (len(filename),)
+                    self._socket.sendall(intPacker.pack(*value))
+                    self._socket.sendall(filename)
+                    
+                    base64File = base64.b64encode(fileInBytes)
+                    value = (len(base64File), )
+                    self._socket.sendall(intPacker.pack(*value))
+                    self._socket.sendall(base64File)
+
+                    os.remove(directoryToScan + filename)
+
+            time.sleep(3)
+        logger.info('File Relay Thread stopped')
+
+    def stop(self):
+        self._stop.set()
+   
+    def stopped(self):
+        return self._stop.isSet()
+
