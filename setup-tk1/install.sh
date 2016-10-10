bf677950b880b3b47a89096961ce1630ef03d2c1
Jeff Maggio <jmaggio14@gmail.com>
2016-10-10 00:45:31 -0400

    adds feh to installation-we needed an image viewer

--- setup-tk1/install.sh
+++ setup-tk1/install.sh
@@ -21,7 +21,7 @@ apt-add-repository multiverse
 apt-get update
 
 #install essentials
-apt-get install git vlc build-essential checkinstall curl libssl-dev tightvncserver python-pip python-dev python-imaging-tk libvlc-dev libvlc5 libgtk2.0 gnome-devel
+apt-get install feh git vlc build-essential checkinstall curl libssl-dev tightvncserver python-pip python-dev python-imaging-tk libvlc-dev libvlc5 libgtk2.0 gnome-devel
 
 
 #install vlc and node
