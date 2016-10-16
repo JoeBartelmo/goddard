1f8b84ca15524108c66c0879d61feac1ccca0b01
Joe Bartelmo <joebartelmo@gmail.com>
2016-10-16 15:56:41 -0400

    Fix majority of GUI errors that Tyler found

--- gui/TopMenu.py
+++ gui/TopMenu.py
@@ -40,9 +40,12 @@ class TopMenu(tk.Menu):
         self.command_menu = tk.Menu(self, tearoff = 0)
         self.client_menu = tk.Menu(self, tearoff = 0)
         self.init_ui(config_file, name)
-        self.existing_roots = {'Mars Commands': self.command_menu}
+        self.existing_roots = {'Mars Commands': { 'parent': None, 'menu': self.command_menu }}
 
     def init_ui(self, config_file, name):
+        '''
+        Pulls in all commands from operations.json
+        '''
         commands_config = self.load_commands(config_file)
 
         for key, val in commands_config.iteritems():
@@ -56,33 +59,79 @@ class TopMenu(tk.Menu):
                     options.add_command(label=opt, command=self.commandFunc(copyval, opt))
                 self.command_menu.add_cascade(label=key, menu=options)
 
-    def add_menu_item(self, commandName, function, menuRoot = 'Client Commands'):
+    def get_parent_tree(self, i, roots):
+        delim = '/'
+        arr = roots[0:(i + 1)]
+        return delim.join(arr)
+
+    def add_menu_item(self, commandName, function, menuRoot = None):
         '''
         Adds a new menu item to the TopMenu. 
         name: Displayed name of the command
         function: callback function assigned when button is clicked
         root: MenuItem to append the new operation under
-            client: Client Commands item
-            server: Server Commands Item
             None: Root level
+            Else: For example: View/blah will make a menu item "View" and append "blah" underneath with the given function.
         '''
         if menuRoot is not None:
-            if menuRoot in self.existing_roots:
-               self.existing_roots[menuRoot].add_command(label=commandName, command=function)
-            else:
+            roots = menuRoot.split('/')
+            #establish root exists
+            if roots[0] not in self.existing_roots:
+                print 'adding', roots[0]
                 menu = tk.Menu(self, tearoff = 0)
-                menu.add_command(label = commandName, command = function)
-                self.existing_roots[menuRoot] = menu
+                self.existing_roots[roots[0]] = {'parent': None, 'menu' : menu}
+            
+            #make all menus up to given function
+            for i in range(1, len(roots)):
+                root = roots[i]
+                root_parent = self.get_parent_tree(i, roots)
+                if root_parent not in self.existing_roots:
+                    print 'adding', root_parent
+                    menu = tk.Menu(self, tearoff = 0)
+                    self.existing_roots[root_parent] = {'parent': self.get_parent_tree(i - 1, roots), 'menu' : menu}
+            
+            #append our function to the last root
+            self.existing_roots[menuRoot]['menu'].add_command(label = commandName, command = function)
         else:
             self.add_command(label=commandName, command = function)
 
+    def get_highest_delim(self):
+        maxSize = 0
+        keys = []
+        for key in self.existing_roots:
+            rootSize = len(key.split('/'))
+            if rootSize > maxSize:
+                keys = [key]
+                maxSize = rootSize
+            elif rootSize == maxSize:
+                keys.append(key)
+        return (maxSize, keys)
+
+    def get_parent_menu_tuple(self, key):
+        return self.existing_roots[key]['parent'], self.existing_roots[key]['menu']
+
     def finalize_menu_items(self):  
         '''
-        Adds the menu items this menu
+        Adds all existing_roots and submenus to root
+        We do this after all add_menu_items are called
+        because if we do it on add, then the first add
+        is the final add, so we need versatility
         '''
         #updates topmenu with given command_menu
+        size, keys = self.get_highest_delim()
+        print size, keys
+        while size > 1:
+            #go through each non-root menu and adds them to their parent
+            for key in keys:
+                parent, key_menu = self.get_parent_menu_tuple(key)
+                print 'adding', key, 'to', parent
+                self.existing_roots[parent]['menu'].add_cascade(label = key.replace(parent + '/', ''), menu = key_menu)
+                self.existing_roots.pop(key, None)
+            size, keys = self.get_highest_delim()
+
+        # we now only have roots, add them all to self
         for key in self.existing_roots:
-            self.add_cascade(label=key , menu=self.existing_roots[key])
+            self.add_cascade(label = key, menu = self.existing_roots[key]['menu'])
     
     def commandFunc(self, cmd, option=False):
         '''
